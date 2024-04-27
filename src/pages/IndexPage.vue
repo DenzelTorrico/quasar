54bcd40d8a06629e8de26fb573f98aa9675ca32f
DenzelTorrico <denzel_silva@hotmail.com>
2024-04-26 23:11:30 -0500

    index

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row q-pa-md">
+    <q-label class="col-md-12 text-h3 q-mb-md">Formulario</q-label>
+    <q-input outlined class="col-md-6"  label="nombre" :dense="dense" />
+    <q-input outlined class="col-md-6"  label="Apellidos" :dense="dense" />
+    <q-btn color="white" text-color="black" label="Standard" />
+
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
